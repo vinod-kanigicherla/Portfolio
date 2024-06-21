9cdd8daad2c6257f51de90cc6ff7ace88d742deb
Vinod Kanigicherla <vinod.kanigicherla@gmail.com>
2024-06-21 14:09:34 -0500

    Added about me + styling

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,15 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    context = {
+        "title": "Vinod Kanigicherla",
+        "about_me": "Hi! I'm Vinod, a student at UCSB majoring in Statistics and Data Science. I am deeply interested in the intersection of data science, computer science, and software engineering, with a particular focus on machine learning and its practical applications for solving real-world challenges.",
+        "url": os.getenv("URL")
+    }
+
+    return render_template('index.html', **context)
+
+if __name__ == '__main__':
+    app.run(debug=True)
