85c493233f5ef216889810571a993590b7c3eb41
Vinod Kanigicherla <vinod.kanigicherla@gmail.com>
2024-07-26 19:43:34 +0000

    Add redeploy script for Docker

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-cd /root/Portfolio
+# Change to the project directory
+cd /path/to/your/project
 
+# Fetch the latest changes from the main branch
 git fetch && git reset origin/main --hard
 
-source /root/Portfolio/python3-virtualenv/bin/activate
+# Spin down the containers 
+docker-compose -f docker-compose.prod.yml down
 
-pip3 install -r requirements.txt
+# Build and spin up the containers
+docker-compose -f docker-compose.prod.yml up -d --build
 
-sudo systemctl restart myportfolio
